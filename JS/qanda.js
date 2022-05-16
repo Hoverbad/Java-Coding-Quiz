6fcbd1587b7efcb70b40af76386556a328cc4799
Hoverbad <102932904+Hoverbad@users.noreply.github.com>
2022-05-16 00:59:05 -0500

    Let's try this again
    Re-pushed the index.html with "./JS/xxxxxxx.js"

---
+++ JS/qanda.js
@@ -0,0 +1,114 @@
+
+// The variable element containing the questions, possible choices, and correct answer
+var questions = [
+  {
+    title: "When the value of an operator is NULL, the typeof returned by the unary operator will be a _______",
+    choices: [
+      "Boolean",
+       "Undefined", 
+       "Object", 
+       "Integer"
+      ],
+    answer: "Object"
+  },
+  {
+    title: "The process in which an object or data structure is translated into a format suitable for transferral over a network, or storage is called?",
+    choices: [
+      "Object Serialization",
+      "Object Encapsulation",
+      "Object Inheritance",
+      "None of the above"
+    ],
+    answer: "Object Serialization"
+  },
+  {
+    title: "Commonly used data types DO NOT include:",
+    choices: [
+      "Strings", 
+      "Booleans", 
+      "Alerts", 
+      "Numbers"
+    ],
+    answer: "Alerts"
+  },
+  {
+    title: "Which function is used to serialize an object into a JSON string in Javascript?",
+    choices: [
+      "Stringify",
+      "Parse",
+      "Convert",
+      "None of the Above"
+    ],
+    answer: "Stringify"
+  },
+  {
+    title: "Which of the following are closures in JavaScript?",
+    choices: [
+      "Variables",
+      "Functions",
+      "Objects",
+      "All of the Above"
+    ],
+    answer: "All of the Above"
+  },
+  {
+    title: "What keyword is used to declare an asynchronous function in Javascript?",
+    choices: [
+      "async",
+      "await",
+      "Both",
+      "Neither"
+    ],
+    answer: "async"
+  },
+  {
+    title: "What does the ... operator do in Javascript?",
+    choices: [
+      "It is used to spread iterables to individual elements", 
+      "It is used to describe a datatype", 
+      "it replaces a comma for a millenial", 
+      "Some of the above"
+    ],
+    answer: "It is used to spread iterables to individual elements"
+  },
+  {
+    title: "What object in Javascript doesn't have a prototype",
+    choices: [
+      "Base object",
+      "The Apple II",
+      "All objects have prototypes",
+      "BASED object"
+    ],
+    answer: "Base object"
+  },
+  {
+    title: "Which of the following are not Server-side objects?",
+    choices: [
+      "Date", 
+      "File Upload", 
+      "Function", 
+      "All of the Above"
+    ],
+    answer: "All of the Above"
+  },
+  {
+    title: "Which HTML element stores the Javascript?",
+    choices: [
+     "<JavaScript>", 
+     "<js>", 
+     "<script>", 
+     "<java>"
+  ],
+    answer: "<script>"
+  },
+  {
+    title: "It is possible to nest functions in Javascript",
+    choices: [
+      "True", 
+      "False", 
+      "Maybe", 
+      "All the above"
+  ],
+    answer: "True"
+  }
+];
